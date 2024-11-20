8da6d784ab72152b697f39c17373f52308630133
Skrub <44077242+triplebproductionzz@users.noreply.github.com>
2024-11-19 22:48:44 -0700

    finished deliverable 10

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,168 @@
+import { sleep, check, group, fail } from "k6";
+import http from "k6/http";
+import jsonpath from "https://jslib.k6.io/jsonpath/1.0.2/index.js";
+
+export const options = {
+  cloud: {
+    distribution: { "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: "ramping-vus",
+      gracefulStop: "30s",
+      stages: [
+        { target: 5, duration: "30s" },
+        { target: 15, duration: "1m" },
+        { target: 10, duration: "30s" },
+        { target: 0, duration: "30s" },
+      ],
+      gracefulRampDown: "30s",
+      exec: "scenario_1",
+    },
+  },
+};
+
+export function scenario_1() {
+  let response;
+
+  group("page_2 - https://pizza.bishoff.click/", function () {
+    // Homepage
+    response = http.get("https://pizza.bishoff.click/", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "if-modified-since": "Tue, 29 Oct 2024 20:42:23 GMT",
+        "if-none-match": '"db634f32e1f88bc7e0684f956f318c32"',
+        priority: "u=0, i",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+    });
+    sleep(8.3);
+
+    // Login
+    response = http.put(
+      "https://pizza-service.bishoff.click/api/auth",
+      '{"email":"d@jwt.com","password":"diner"}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.bishoff.click",
+          priority: "u=1, i",
+          "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    if (
+      !check(response, { "status equals 200": (response) => response.status.toString() === "200" })
+    ) {
+      console.log(response.body);
+      fail("Login was *not* 200");
+    }
+    let token;
+    token = jsonpath.query(response.json(), "$.token")[0];
+    sleep(8.6);
+
+    // Menu
+    response = http.get("https://pizza-service.bishoff.click/api/order/menu", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "if-none-match": 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: "https://pizza.bishoff.click",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+
+    // Franchise
+    response = http.get("https://pizza-service.bishoff.click/api/franchise", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "if-none-match": 'W/"7b-lyD/x1Pw/OF009Mm2hIdv1GB8+M"',
+        origin: "https://pizza.bishoff.click",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+    sleep(5.3);
+
+    // Purchase Pizza
+    response = http.post(
+      "https://pizza-service.bishoff.click/api/order",
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"2","franchiseId":1}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.bishoff.click",
+          priority: "u=1, i",
+          "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    let jwt;
+    jwt = jsonpath.query(response.json(), "$.jwt")[0];
+    sleep(3.2);
+
+    // Verify pizza
+    response = http.post("https://pizza-factory.cs329.click/api/order/verify", `{"jwt": ${jwt}}`, {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        origin: "https://pizza.bishoff.click",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+      },
+    });
+  });
+}
